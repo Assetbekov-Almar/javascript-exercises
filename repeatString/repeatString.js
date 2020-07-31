3c675aca216dcf2c60ab9f26fde8d64dcc096e05
Almar <almar.asetbekov@gmail.com>
2020-07-31 16:02:50 +0600

    Strings

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(str,num) {
+if (num < 0) return "ERROR";
+let i = 0;
+let word = "";
+while (i < num) {
+word += str;
+i++;
+}
+return word;
 }
 
 module.exports = repeatString
--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+// let splitedStr = string.split();
+// let ans = "";
+// for (let i = string.length; i > 0 ; i--) {
+//    ans += splitedStr[i];
+// }
+// return ans;
+return string.split("").reverse().join("");
 }
 
 module.exports = reverseString
